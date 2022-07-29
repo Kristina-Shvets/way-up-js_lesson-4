c5aaac6fec8552c05132bfd2966a4b69333feffe
unknown <sveckristina16@gmail.com>
2022-07-29 19:45:41 +0300

    My commit

---
+++ wpdmn_lesson4/slider_new/main.js
@@ -0,0 +1,68 @@
+const prev=document.getElementById('btn-prev');
+const next=document.getElementById('btn-next');
+const slides=document.querySelectorAll('.slide');
+const dots=document.querySelectorAll('.dot');
+
+let index = 0;
+// console.log(n); 
+
+const activeSlide = n =>{
+    for(let slide of slides){
+        // console.log(slide)
+        slide.classList.remove('active')
+    }
+    slides[n].classList.add('active')
+}
+
+
+
+
+const prepareCurrentSlide=ind=>{
+    activeSlide(index);
+    activeDot(index);
+}
+
+const activeDot = n =>{
+    for(let dot of dots){
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active')
+}
+
+const nextSlide = ()=>{
+    if(index == slides.length-1){
+        index = 0;
+        prepareCurrentSlide(index)
+    }else{
+        index++;
+        prepareCurrentSlide(index);
+    }
+    
+}
+
+setInterval(()=>{
+    nextSlide()
+}, 1000);
+const prevSlide=()=>{
+    if(index == 0){
+        index = slides.length-1;
+        prepareCurrentSlide(index);
+    }else{
+        index --;
+        prepareCurrentSlide(index);
+    }
+}
+
+
+dots.forEach((item, indexDot)=>{
+    item.addEventListener('click',()=>{
+        index=indexDot;
+        prepareCurrentSlide(index);
+    })
+    
+   
+})
+
+
+next.addEventListener('click' ,nextSlide);
+prev.addEventListener('click',prevSlide);
---
+++ wpdmn_tabs/tabs/main.js
@@ -0,0 +1,24 @@
+const tabs=document.getElementById('tabs');
+const content=document.querySelectorAll('.content')
+
+
+const changeClass=el=>{
+    
+    for(let i=0; i< tabs.children.length; i++){
+        tabs.children[i].classList.remove('active');
+    }
+    el.classList.add('active');
+}
+
+
+tabs.addEventListener('click', e =>{
+    const currentTub =e.target.dataset.btn;
+    changeClass(e.target)
+    for(let i=0; i< content.length; i++){
+       content[i].classList.remove('active');
+      
+       if(content[i].dataset.content ==currentTub) {
+            content[i].classList.add('active')
+       }
+    }
+}) 
